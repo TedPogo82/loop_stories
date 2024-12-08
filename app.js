2f66212d99b36153c5849561d7165afbd05b18f3
root <root@DESKTOP-5N0DJHD>
2024-12-08 15:02:16 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById("submit").addEventListener("click", function () {
+  //correct answer
+  const correctAnswer = "ESCAPE";
+
+  //get user input
+  const userAnswer = document.getElementById("answer").value.trim();
+
+  //get the result
+  const result = document.getElementById("result");
+
+  //check conditional
+  if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+    window.location.href = "escape.html";
+  } else {
+    alert("Wrong answer, try again!");
+  }
+});
